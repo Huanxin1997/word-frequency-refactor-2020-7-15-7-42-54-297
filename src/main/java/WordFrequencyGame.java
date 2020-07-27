8505c95fd2aef7ba4e8bce7b9d801eef2532f022
Hans <741581879@qq.com>
2020-07-27 15:42:06 +0800

    [Hans] refactor: Extract constant CALCULATE_ERROR_MESSAGE

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -9,6 +9,7 @@ public class WordFrequencyGame {
     public static final String SPACE_PATTERN = "\\s+";
     public static final String WRAP_PATTERN = "\n";
     public static final String SINGLE_SPACE = " ";
+    public static final String CALCULATE_ERROR_MESSAGE = "Calculate Error";
 
     public String getResult(String inputStr) {
 
@@ -42,7 +43,7 @@ public class WordFrequencyGame {
                 }
                 return joiner.toString();
             } catch (Exception e) {
-                return "Calculate Error";
+                return CALCULATE_ERROR_MESSAGE;
             }
         }
     }
