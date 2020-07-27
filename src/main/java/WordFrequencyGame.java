f2a8ddcf78a9fe177110b88c9fcd534ed69df5e2
Hans <741581879@qq.com>
2020-07-27 16:05:17 +0800

    [Hans] refactor: Rename list to tempWordInfos

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -27,12 +27,12 @@ public class WordFrequencyGame {
 
                 Map<String, List<WordInfo>> map = getListMap(wordInfos);
 
-                List<WordInfo> list = new ArrayList<>();
+                List<WordInfo> tempWordInfos = new ArrayList<>();
                 for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()) {
                     WordInfo input = new WordInfo(entry.getKey(), entry.getValue().size());
-                    list.add(input);
+                    tempWordInfos.add(input);
                 }
-                wordInfos = list;
+                wordInfos = tempWordInfos;
 
                 wordInfos.sort((firstWordInfo, secondWordInfo) -> secondWordInfo.getWordCount() - firstWordInfo.getWordCount());
 
